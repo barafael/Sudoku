5ed4feec3ed03636f74313bf87e20e171bba2b8e
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-23 16:54:43 +0200

    addtest stub class

---
+++ src/sudoku/game/SudokuGameTest.java
@@ -0,0 +1,28 @@
+package sudoku.game;
+
+/**
+ * Created by ra on 23.06.16.
+ * Part of Sudoku, in package sudoku.game.
+ */
+public class SudokuGameTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void setValue() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getColumn() throws Exception {
+
+    }
+
+}
