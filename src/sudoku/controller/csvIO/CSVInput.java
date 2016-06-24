31e5734e55aac17aa1c2b5ec3c8413bb07cf817d
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-24 20:10:55 +0200

    add warning dialogs and correct a mistake where 0 wouldn't be validated correctly. It is allowed as poison value.

--- src/sudoku/controller/csvIO/CSVInput.java
+++ src/sudoku/controller/csvIO/CSVInput.java
@@ -1,6 +1,7 @@
 package sudoku.controller.csvIO;
 
 import com.opencsv.CSVReader;
+import javafx.scene.control.Alert;
 import sudoku.game.SudokuGame;
 
 import java.io.File;
@@ -16,30 +17,38 @@ import java.util.List;
  */
 public class CSVInput {
     /**
-     * Method to load a .csv file located in assets/csv in a game.
-     *  @param game     game field to be filled with csv entries
+     * Method to load a .csv file.
+     *
+     * @param game game field to be filled with csv entries
      * @param file csv file containing all entries, directly mapped to a sudoku field
      */
     public void loadCSV(SudokuGame game, File file) {
         try (CSVReader csvReader = new CSVReader(new FileReader(file), ';')) {
             List<String[]> lines = csvReader.readAll();
             List<Byte[]> rows = parseStringLines(lines);
-            if(csvValidate(game.getSize(), rows)) {
+            if (csvValidate(game.getSize(), rows)) {
                 for (byte rowIndex = 0; rowIndex < game.getSize(); rowIndex++) {
                     game.setRow(rowIndex, rows.get(rowIndex));
                 }
             }
         } catch (NumberFormatException e) {
-            System.err.println("There was a non-number in the csv file!");
-            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Invalid file content!");
+            alert.setContentText("The file you tried to open contained invalid input!");
+            alert.showAndWait();
         } catch (IOException e) {
-            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Invalid file!");
+            alert.setContentText("There was an error during opening of the file you requested.");
+            alert.showAndWait();
         }
     }
 
     private List<Byte[]> parseStringLines(List<String[]> lines) throws NumberFormatException {
         List<Byte[]> rows = new ArrayList<>(lines.size());
-        for (String[] strings : lines ) {
+        for (String[] strings : lines) {
             Byte[] row = new Byte[strings.length];
             for (int i = 0; i < strings.length; i++) {
                 row[i] = Byte.parseByte(strings[i]);
@@ -51,7 +60,7 @@ public class CSVInput {
 
     private static boolean csvValidate(byte size, List<Byte[]> rows) {
         boolean correctLength = rows.stream().allMatch(byteArr -> byteArr.length == size);
-        boolean correctRange  = rows.stream().allMatch(bytes -> Arrays.stream(bytes).allMatch(b -> b>=0 && b<=size));
+        boolean correctRange = rows.stream().allMatch(bytes -> Arrays.stream(bytes).allMatch(b -> b >= 0 && b <= size));
         return correctLength && correctRange;
     }
 }
