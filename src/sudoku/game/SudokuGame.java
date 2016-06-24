588364318622db02889a9dbb26aefe1135a48ec4
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-24 14:06:07 +0200

    add various setters and checkers and tests for them

--- src/sudoku/game/SudokuGame.java
+++ src/sudoku/game/SudokuGame.java
@@ -1,21 +1,29 @@
 package sudoku.game;
 
 import java.util.Arrays;
+import java.util.Observable;
 
 /**
  * Created by ra on 23.06.16.
  * Part of Sudoku, in package sudoku.game.solver.
  */
-public class SudokuGame {
-    // Using bytes because an int is 4 times the size. So, for 9x9 = 81, that would be 324 bytes for int, but only 81
-    // for byte.
-    // Sudoku games with more than 127 rows/cols aren't going to be solved that soon anyway.
+public class SudokuGame extends Observable {
+    /* Using byte type because an int is 4 times the size. So, for 9x9 = 81, that would be 324 bytes for int,
+    but only 81 for byte.
+    Sudoku games with more than 127 rows/cols aren't going to be solved that soon anyway.
+    Also using simple 2-d array. Arrays can hold primitive types, collections cannot.
+    Arrays have O(1) indexing and changing values. Size of field is constant.
+    */
     private static final byte SIZE = 9;
-    byte[][] field = new byte[SIZE][SIZE];
+    private final byte[][] field = new byte[SIZE][SIZE];
 
     { // initialize field with poison value 0(which indicates no value set).
         for (int i = 0; i < SIZE; i++)
             Arrays.fill(field[i], (byte) 0); // compile time cast; inconvenient, but has to be done.
+    }
+
+    public SudokuGame() {
+        assert isPerfectSquare(SIZE);
     }
 
     /**
@@ -29,28 +37,100 @@ public class SudokuGame {
      * @return null if invalid args,the game otherwise.
      */
     public SudokuGame setValue(byte row, byte col, byte val) {
-        //@formatter:off
         if (row < SIZE && row >= 0 &&
-            col < SIZE && col >= 0 &&
-            val <= SIZE &&  val > 0) { // valid arguments
-            // formatter:on
+                col < SIZE && col >= 0 &&
+                val <= SIZE && val > 0) { // valid arguments?
             field[row][col] = val;
+            setChanged();
+            notifyObservers();
             return this; // enable chaining
         } else {
             return null;
         }
     }
-    /** return an array that is the column at colIndex. Changes don't write through.
-     * For that, the setValue() method has to be used.
-     * The returned array contains a 0 value if no value has been set.
-     * @param colIndex
-     * @return
+
+    /**
+     * helper method to enable calling setValue with ints or literal values. Preferably unused.
+     *
+     * @param row 0..SIZE-1
+     * @param col 0..SIZE-1
+     * @param val 1..SIZE
+     * @return null if invalid arguments, this instance otherwise.
      */
-    public byte[] getColumn(final byte colIndex) {
+    public SudokuGame setValue(int row, int col, int val) {
+        return setValue((byte) row, (byte) col, (byte) val);
+    }
+
+
+    /**
+     * Returns if the specified column contains the specified value.
+     *
+     * @param colIndex 0..SIZE-1
+     * @return true if column at colIndex contains value.
+     */
+    public boolean columnContains(final byte value, final byte colIndex) {
+        for (int rowIndex = 0; rowIndex < SIZE; rowIndex++) {
+            if (field[rowIndex][colIndex] == value)
+                return true;
+        }
+        return false;
+    }
+
+    public byte[] getColumn(byte colIndex) {
         byte[] column = new byte[SIZE];
-        for(int rowIndex = 0; rowIndex < SIZE; rowIndex++) {
+        for (int rowIndex = 0; rowIndex < SIZE; rowIndex++) {
             column[rowIndex] = field[rowIndex][colIndex];
         }
         return column;
+    }
+
+    /**
+     * Returns if the specified row contains the specified value.
+     *
+     * @param rowIndex 0..SIZE-1
+     * @return true if row at rowIndex contains value.
+     */
+    public boolean rowContains(final byte value, final byte rowIndex) {
+        for (int colIndex = 0; colIndex < SIZE; colIndex++) {
+            if (field[rowIndex][colIndex] == value)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns if the specified square contains the given value.
+     *
+     * @param rowIndex index of the row the square starts on divided by sqrt(SIZE)
+     *                 I.e., to get to the middle row in a sudoku of 9,
+     *                 rowIndex would be 1
+     * @param colIndex index of the column the square starts on divided by sqrt(SIZE)
+     *                 I.e., to get to the first column in a sudoku of 9,
+     *                 colIndex would be 0
+     * @return true if value present in specified square.
+     */
+    public boolean squareContains(final byte value, final byte rowIndex, final byte colIndex) {
+        if (rowIndex * rowIndex > SIZE || colIndex * colIndex > SIZE) {
+            throw new IllegalArgumentException();
+        }
+        byte blockSize = (byte) Math.sqrt(SIZE);
+        byte[][] block = new byte[blockSize][blockSize];
+        for (int row = rowIndex * blockSize; row < rowIndex * blockSize + blockSize; row++) {
+            for (int col = colIndex * blockSize; col < colIndex * blockSize + blockSize; col++) {
+                if (field[row][col] == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isPerfectSquare(int number) {
+        if (number > 0) { // 0 is explicitly excluded
+            long temp = (long) (Math.sqrt(number));
+            return temp * temp == number;
+        } else {
+            return false;
+        }
     }
 }
--- src/sudoku/game/SudokuGameTest.java
+++ src/sudoku/game/SudokuGameTest.java
@@ -1,28 +1,48 @@
 package sudoku.game;
 
+import java.util.Arrays;
+
 /**
  * Created by ra on 23.06.16.
  * Part of Sudoku, in package sudoku.game.
  */
 public class SudokuGameTest {
+    SudokuGame game;
+
     @org.junit.Before
     public void setUp() throws Exception {
-
+        game = new SudokuGame();
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
-
+        game = null;
     }
 
     @org.junit.Test
     public void setValue() throws Exception {
-
+        game.setValue(1, 0, 5);
+        byte[] zeroColumn = new byte[]{0, 5, 0, 0, 0, 0, 0, 0, 0};
+        assert Arrays.equals(zeroColumn, game.getColumn((byte) 0));
+        System.out.println("tests run!");
     }
 
     @org.junit.Test
     public void getColumn() throws Exception {
-
+        game.setValue(0, 0, 1);
+        game.setValue(8, 0, 1);
+        game.setValue(0, 8, 3);
+        assert Arrays.equals(game.getColumn((byte) 0), new byte[]{1, 0, 0, 0, 0, 0, 0, 0, 1});
+        assert Arrays.equals(game.getColumn((byte) 8), new byte[]{3, 0, 0, 0, 0, 0, 0, 0, 0});
     }
 
+    @org.junit.Test
+    public void columnContains() throws Exception {
+        game.setValue(0, 1, 1);
+        game.setValue(8, 1, 3);
+        game.setValue(3, 1, 5);
+        assert game.columnContains((byte) 3, (byte) 1);
+        assert game.columnContains((byte) 5, (byte) 1);
+        assert game.columnContains((byte) 0, (byte) 8);
+    }
 }
