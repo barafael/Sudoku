6925758f89d64ba0f42a5f9d31c921b70c6cb95f
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-24 20:08:48 +0200

    add csv output functionality

--- src/sudoku/controller/Controller.java
+++ src/sudoku/controller/Controller.java
@@ -15,14 +15,18 @@ public class Controller {
     CSVInput loader;
     CSVOutput writer;
 
-    public Controller (SudokuGame game, Stage primaryStage) {
+    public Controller(SudokuGame game, Stage primaryStage) {
         this.game = game;
         this.primaryStage = primaryStage;
+        loader = new CSVInput();
+        writer = new CSVOutput();
     }
+
     @FXML
     public void initialize() {
 
     }
+
     public void loadCSVFile() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
@@ -31,8 +35,21 @@ public class Controller {
                 new FileChooser.ExtensionFilter("Text Files", "*.txt"),
                 new FileChooser.ExtensionFilter("All Files", "*.*"));
         File selectedFile = fileChooser.showOpenDialog(primaryStage);
-        CSVInput loader = new CSVInput();
-        loader.loadCSV(game, selectedFile);
-        System.out.println(game.toString());
+        if (selectedFile != null) {
+            loader.loadCSV(game, selectedFile);
+        }
+    }
+
+    public void writeCSVFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("CSV Files", "*.csv"),
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+        File selectedFile = fileChooser.showSaveDialog(primaryStage);
+        if (selectedFile != null) {
+            writer.writeCSV(game, selectedFile);
+        }
     }
 }
--- src/sudoku/controller/csvIO/CSVOutput.java
+++ src/sudoku/controller/csvIO/CSVOutput.java
@@ -1,8 +1,34 @@
 package sudoku.controller.csvIO;
 
+import javafx.scene.control.Alert;
+import sudoku.game.SudokuGame;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 /**
  * Created by ra on 24.06.16.
  * Part of Sudoku, in package sudoku.controller.csvIO.
  */
 public class CSVOutput {
+    /**
+     * Method to write a .csv file.
+     *
+     * @param game game field to be filled with csv entries
+     * @param file csv file containing all entries, directly mapped to a sudoku field
+     */
+    public void writeCSV(SudokuGame game, File file) {
+        try {
+            Files.write(file.toPath(), game.toString().getBytes());
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setHeaderText("Invalid file!");
+            alert.setContentText("There was an error during writing to the file you requested.");
+            alert.showAndWait();
+        }
+    }
 }
+
+
