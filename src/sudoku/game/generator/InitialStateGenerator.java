4314b04b072a571903d9067086fadff433db1a82
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-25 13:39:43 +0200

    add initialStateGenerator stub

---
+++ src/sudoku/game/generator/InitialStateGenerator.java
@@ -0,0 +1,11 @@
+package sudoku.game.generator;
+
+/**
+ * Created by ra on 25.06.16.
+ * Part of Sudoku, in package sudoku.game.generator.
+ */
+public class InitialStateGenerator {
+    public static int[][] generateInitialState(int size) {
+        return new int[size][size]; // TODO generate initial state // STOPSHIP: 25.06.16
+    }
+}
