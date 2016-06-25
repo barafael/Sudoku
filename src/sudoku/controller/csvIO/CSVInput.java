9d560bd44f420e8f315d47a2ed5dbf622d226b89
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-25 13:42:00 +0200

    adjust file input to new constructor for controller

--- src/sudoku/controller/csvIO/CSVInput.java
+++ src/sudoku/controller/csvIO/CSVInput.java
@@ -2,7 +2,7 @@ package sudoku.controller.csvIO;
 
 import com.opencsv.CSVReader;
 import javafx.scene.control.Alert;
-import sudoku.game.SudokuGame;
+import sudoku.game.SudokuBoard;
 
 import java.io.File;
 import java.io.FileReader;
@@ -19,17 +19,23 @@ public class CSVInput {
     /**
      * Method to load a .csv file.
      *
-     * @param game game field to be filled with csv entries
-     * @param file csv file containing all entries, directly mapped to a sudoku field
+     * @param file CSV file containing all entries, directly mapped to a sudoku field
+     * @return a sudoku board with entries from file
      */
-    public void loadCSV(SudokuGame game, File file) {
+    public SudokuBoard loadCSV(File file) {
         try (CSVReader csvReader = new CSVReader(new FileReader(file), ';')) {
             List<String[]> lines = csvReader.readAll();
-            List<Byte[]> rows = parseStringLines(lines);
-            if (csvValidate(game.getSize(), rows)) {
-                for (byte rowIndex = 0; rowIndex < game.getSize(); rowIndex++) {
-                    game.setRow(rowIndex, rows.get(rowIndex));
+            List<Integer[]> rows = parseStringLines(lines);
+            int size = checkDimensions(rows);
+            if (csvValidate(size, rows)) {
+                int[][] newBoard = new int[size][size];
+                for (int rowIndex = 0; rowIndex < size; rowIndex++) {
+                    Integer[] row = rows.get(rowIndex);
+                    for (int colIndex = 0; colIndex < row.length; colIndex++) {
+                        newBoard[rowIndex][colIndex] = row[colIndex];
+                    }
                 }
+                return new SudokuBoard(newBoard);
             }
         } catch (NumberFormatException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -44,23 +50,38 @@ public class CSVInput {
             alert.setContentText("There was an error during opening of the file you requested.");
             alert.showAndWait();
         }
+        return null;
     }
 
-    private List<Byte[]> parseStringLines(List<String[]> lines) throws NumberFormatException {
-        List<Byte[]> rows = new ArrayList<>(lines.size());
+    private int checkDimensions(List<Integer[]> rows) {
+        int length = rows.get(0).length;
+        boolean correct = true;
+        for (Integer[] integers: rows) {
+            if (integers.length != length) {
+                correct = false;
+            }
+        }
+        if(rows.size() != length) { // not square
+            correct = false;
+        }
+        return correct ? length : 0;
+    }
+
+    private List<Integer[]> parseStringLines(List<String[]> lines) throws NumberFormatException {
+        List<Integer[]> rows = new ArrayList<>(lines.size());
         for (String[] strings : lines) {
-            Byte[] row = new Byte[strings.length];
+            Integer[] row = new Integer[strings.length];
             for (int i = 0; i < strings.length; i++) {
-                row[i] = Byte.parseByte(strings[i]);
+                row[i] = Integer.parseInt(strings[i]);
             }
             rows.add(row);
         }
         return rows;
     }
 
-    private static boolean csvValidate(byte size, List<Byte[]> rows) {
-        boolean correctLength = rows.stream().allMatch(byteArr -> byteArr.length == size);
-        boolean correctRange = rows.stream().allMatch(bytes -> Arrays.stream(bytes).allMatch(b -> b >= 0 && b <= size));
+    private static boolean csvValidate(int size, List<Integer[]> rows) {
+        boolean correctLength = rows.stream().allMatch(intArr -> intArr.length == size);
+        boolean correctRange = rows.stream().allMatch(ints -> Arrays.stream(ints).allMatch(i -> (i >= 0) && (i <= size)));
         return correctLength && correctRange;
     }
 }
