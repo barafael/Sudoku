ba7c0e5449be8ac2c0ce8917da82d5c4a10e0f2a
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-29 14:03:42 +0200

    add css

--- src/sudoku/Main.java
+++ src/sudoku/Main.java
@@ -6,21 +6,31 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sudoku.controller.Controller;
+import sudoku.game.SudokuBoard;
 
 public class Main extends Application {
+
+    private static final int WIDTH = 720;
+    private static final int HEIGHT = 1080;
+    private static SudokuBoard game;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("view/sudoku.fxml"));
-        loader.setControllerFactory(c -> new Controller(primaryStage));
-        setUserAgentStylesheet(STYLESHEET_MODENA);
+        loader.setControllerFactory(c -> new Controller(primaryStage, game));
         Parent root = loader.load();
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        setUserAgentStylesheet(STYLESHEET_MODENA);
+        scene.getStylesheets()
+                .add(getClass().getResource("view/sample.css")
+                        .toExternalForm());
         primaryStage.setTitle("Sudoku");
-        primaryStage.setScene(new Scene(root, 600, 450));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
+        game = new SudokuBoard();
         launch(args);
     }
 }
