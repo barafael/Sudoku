e4d08721850be3cb1f25866a739a5a3e5e79a0a6
Rafael Bachmann <triolenzunge@gmail.com>
2016-07-01 10:11:30 +0200

    package access for util functions, and offload validPosition here

--- src/sudoku/game/SudokuUtil.java
+++ src/sudoku/game/SudokuUtil.java
@@ -11,20 +11,21 @@ public class SudokuUtil {
      * @param colIndex 0..SIZE-1
      * @return true if column at colIndex contains value.
      */
-    public static boolean colContains(final int[][] board, int size, final int colIndex, final int value) {
+    static boolean colContains(final int[][] board, int size, final int colIndex, final int value) {
         for (int rowIndex = 0; rowIndex < size; rowIndex++) {
             if (board[rowIndex][colIndex] == value)
                 return true;
         }
         return false;
     }
-   /**
+
+    /**
      * Returns if the specified row contains the specified value.
      *
      * @param rowIndex 0..SIZE-1
      * @return true if row at rowIndex contains value.
      */
-    public static boolean rowContains(final int[][] board, int size, final int rowIndex, final int value) {
+    static boolean rowContains(final int[][] board, int size, final int rowIndex, final int value) {
         for (int colIndex = 0; colIndex < size; colIndex++) {
             if (board[rowIndex][colIndex] == value)
                 return true;
@@ -39,7 +40,7 @@ public class SudokuUtil {
      * @param col index of a col in the square
      * @return true if val present in specified square.
      */
-    public static boolean squareContains(int[][] board, int size, int row, int col, int value) {
+    static boolean squareContains(int[][] board, int size, int row, int col, int value) {
         if (row < size && row >= 0 &&
                 col < size && row >= 0 &&
                 value <= size && value >/*=*/ 0) {
@@ -59,12 +60,18 @@ public class SudokuUtil {
         } else return false;
     }
 
-    public static boolean isPerfectSquare(int number) {
+    static boolean isPerfectSquare(int number) {
         if (number > 0) { // 0 is explicitly excluded
             int temp = (int) (Math.sqrt(number));
             return temp * temp == number;
         } else {
             return false;
         }
+    }
+
+    public static boolean validPosition(int[][] board, int size, int row, int col, int value) {
+        return value == 0 ||
+                (!rowContains(board, size, row, value) && !colContains(board, size, col, value) &&
+                        !squareContains(board, size, row, col, value));
     }
 }
