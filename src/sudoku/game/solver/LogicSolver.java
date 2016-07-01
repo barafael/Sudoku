52048c87e1e4cf28640ea5423fec1fae4e8833c5
Rafael Bachmann <triolenzunge@gmail.com>
2016-07-01 23:41:14 +0200

    logic solver implementation.

---
+++ src/sudoku/game/solver/LogicSolver.java
@@ -0,0 +1,85 @@
+package sudoku.game.solver;
+
+import sudoku.game.SudokuUtil;
+
+/**
+ * Created by ra on 01.07.16.
+ * Part of Sudoku, in package sudoku.game.solver.
+ */
+public class LogicSolver {
+    /**
+     * This solver finds places where only one number is possible, and falls back on backtracking when none found.
+     */
+    public static boolean solve(int[][] board, int size) {
+        if (solveHelper(board, size)) {
+            return true;
+        } else {
+            int[][] copy = new int[size][size];
+            for (int row = 0; row < size; row++) {
+                System.arraycopy(board[row], 0, copy[row], 0, size);
+            }
+            return BacktrackSolver.solve(board, copy, size);
+        }
+    }
+
+    private static boolean solveHelper(int[][] board, int size) {
+        boolean change = true;
+        while (!isFull(board) && change) {
+            change = false;
+            boolean[][][] occurrences = new boolean[size][size][size];
+            for (int row = 0; row < size; row++) {
+                for (int col = 0; col < size; col++) {
+                    if (board[row][col] == 0) {
+                        for (int value = 0; value < size; value++) {
+                            occurrences[row][col][value] = occurs(board, size, row, col, value + 1);
+                        }
+                        int value = onlyOne(occurrences[row][col]);
+                        if (value == -1) {
+                            System.err.println("no vacant slots!");
+                            return false;
+                        } else if (value > 0) {
+                            board[row][col] = value;
+                            change = true;
+                        }
+
+                    }
+                }
+            }
+        }
+        // if board is not full, there was a non-unique situation. Probably, the board has still been partially solved
+        return isFull(board);
+    }
+
+    private static boolean isFull(int[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board.length; col++) {
+                if (board[row][col] == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private static int onlyOne(boolean[] booleans) {
+        int count = 0;
+        int value = -1;
+        for (int index = 0; index < booleans.length; index++) {
+            if (!booleans[index]) { // booleans is false at index
+                count++;
+                value = index + 1;
+            }
+        }
+        if (count == 1) { // there was only one!
+            return value;
+        } else if (count == 0) { // all numbers taken, invalid board!
+            return -1;
+        } else {
+            return 0; // not just one
+        }
+    }
+
+    private static boolean occurs(int[][] board, int size, int row, int col, int value) {
+        return !SudokuUtil.validPosition(board, size, row, col, value);
+    }
+
+}
